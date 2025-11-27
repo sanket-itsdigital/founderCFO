4e47d4d5aee5df91f582072739ccb61bba2e7051
Sanket <aagamfashion@Aagams-MacBook-Air.local>
2025-11-27 10:31:32 +0530

    added static root

--- backend/settings.py
+++ backend/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = "static/"
 
 # Directory containing app/static for development
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # (STATIC_ROOT intentionally omitted per user request)
 
