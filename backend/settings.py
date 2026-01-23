9fcf34144878d12d5363be987bbc833d83b5a8bb
Sanket <aagamfashion@Aagams-MacBook-Air.local>
2026-01-23 17:03:28 +0530

    Added admin:subsctiption and user list mens

--- backend/settings.py
+++ backend/settings.py
@@ -196,7 +196,8 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
 
-# LOGIN_URL = "/admin/login/"
+LOGIN_URL = "/api/account/login-admin/"
+LOGIN_REDIRECT_URL = "/"
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
